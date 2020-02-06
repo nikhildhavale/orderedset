355a310f7e2c3a145b052e24bed001e563453d2e
Nikhil <nikhil_dhavale@hotmail.com>
2020-02-06 17:16:27 +0530

    added ordered set

---
+++ OrderedSet/OrderedSet/Person.swift
@@ -0,0 +1,29 @@
+//
+//  Person.swift
+//  OrderedSet
+//
+//  Created by Nikhil d on 06/02/20.
+//  Copyright © 2020 Nikhil d. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var id = ""
+    var name = ""
+    public static func == (lhs: Person, rhs: Person) -> Bool
+    {
+        return lhs.id == rhs.id
+    }
+    override var description: String{
+        return "id = \(id)  name \(name)"
+    }
+    override func isEqual(_ object: Any?) -> Bool {
+        if let person = object as? Person {
+            if person.id == self.id {
+                return true
+            }
+        }
+        return false
+    }
+}
--- OrderedSet/OrderedSet/ViewController.swift
+++ OrderedSet/OrderedSet/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let orderedSet = NSMutableOrderedSet()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let person1 = Person()
+        person1.id = "1"
+        person1.name = "1n"
+        let person2 = Person()
+        person2.id = "2"
+        person2.name = "2n"
+        let person3 = Person()
+        person3.id = "1"
+        person3.name = "1nfff"
+        orderedSet.add(person1)
+        orderedSet.add(person2)
+        for item in orderedSet {
+            if let person = item as? Person {
+                print(person.description)
+            }
+        }
+        print("inserting")
+        orderedSet.add(person3)
+        for item in orderedSet {
+            if let person = item as? Person {
+                print(person.description)
+            }
+        }
     }
 
 
